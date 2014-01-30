83b7fbc66c1caf4343e99562c89123e517604697
ParNurZeal <parnurzeal@gmail.com>
2014-01-31 00:05:19 +0900

    importing the git-demo project

---
+++ src/main/java/com/parnurzeal/jobse/App.java
@@ -0,0 +1,13 @@
+package com.parnurzeal.jobse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
