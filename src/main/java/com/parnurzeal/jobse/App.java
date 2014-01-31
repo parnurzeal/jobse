c10ab2e781d96a5be5df255a6b0ceb5e14cef979
Theeraphol Wattanavekin <parnurzeal@gmail.com>
2014-01-31 21:00:23 +0900

    Try reading from file using Scanner and InputFileStream

--- src/main/java/com/parnurzeal/jobse/App.java
+++ src/main/java/com/parnurzeal/jobse/App.java
@@ -1,13 +1,32 @@
 package com.parnurzeal.jobse;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.apache.commons.io.IOUtils;
+import java.util.Scanner;
 /**
  * Hello world!
  *
  */
 public class App 
 {
+
+    public static String convertStreamToString(java.io.InputStream is){
+      java.util.Scanner s = new Scanner(is).useDelimiter("\\A");
+      return s.hasNext()?s.next():"";
+    }
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        InputStream is = App.class.getResourceAsStream("/rakuten1.job");
+        String text = convertStreamToString(is);
+        System.out.println(text);
+
+      System.out.println( "Hello World!" );
     }
 }
